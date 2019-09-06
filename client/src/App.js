192d88e312ead6b0080a06a5903c0d075f22ae11
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-09-06 11:29:52 -0400

    Rendered data to the console

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state={
+       Data:[]
+     }
+    }
+
+componentDidMount() {
+   fetch('http://localhost:5000/api/players')
+   .then(res => res.json())
+   .then(res => {
+   console.log(res); this.setState({userData: res.data})
+   })
+   .catch(err => console.log('nope'));
+};
+
+
+    render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="App">
+           <h1>Sprint Challenge</h1>
+
+
+
+
+          </div>
   );
+    };
+
 }
 
 export default App;
