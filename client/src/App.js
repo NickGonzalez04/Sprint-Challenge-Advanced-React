66cf968c7d5892670b3132d6b01f64db8fb6ba28
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-09-06 14:33:59 -0400

    Render Player Data to DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
+import axios from 'axios';
+import PlayerList from './components/playerlist.js';
 import './App.css';
 
 class App extends React.Component{
-  constructor() {
+  constructor(){
     super();
-    this.state={
-       Data:[]
+  this.state = {
+       data: []
      }
-    }
+    } 
 
 componentDidMount() {
-   fetch('http://localhost:5000/api/players')
-   .then(res => res.json())
-   .then(res => {
-   console.log(res); this.setState({userData: res.data})
-   })
+   axios.get('http://localhost:5000/api/players')
+  //  .then(res => res.json())
+   .then(data => {
+   console.log('Player data',data); this.setState({data: data.data})})
    .catch(err => console.log('nope'));
 };
 
 
     render() {
+      console.log(this.state.data);
   return (
           <div className="App">
-           <h1>Sprint Challenge</h1>
-
-
-
-
+             <h1>Sprint Challenge</h1>
+          
+           {this.state.data.map(players => {
+             return <PlayerList key={players.id} players={players}/>
+           })}
+          
           </div>
   );
     };
